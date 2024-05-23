0580140ae4245256a70b2a6f8575975a3793d53b
Kevin Yonamine <kyonamine@yext.com>
2024-05-23 14:50:22 -0400

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -8,49 +8,11 @@ def printout():
     f'{firstFile.func()} {secondFile.func()}'
 
 def snowflake():
-    session = get_active_session()
-    f'{session}'
-    return session
-
-
-def connectToSF():
-# def connectToSF(SF_DB_USERNAME, SF_DB_PASSWORD):
-    # try:
-    #     sfConnection = sf.connect(
-    #         account="exa52539.us-east-1",
-    #         user = "APP_TECHNOLOGY_OPERATIONS",
-    #         password=SF_DB_PASSWORD,
-    #         role="TECHNOLOGY",
-    #         # warehouse="PROD_WORKFLOW",
-    #         warehouse="APP_QUERY",
-    #         # database="PROD_PLATFORM_LOCAL")
-    #         database="TEAM_TECHNOLOGY_OPERATIONS")
-    # except:
-    #     SF_DB_USERNAME = "wzuo@yext.com"
-    #     SF_DB_PASSWORD = ""
-    #     sfConnection = sf.connect(
-    #         user=SF_DB_USERNAME,
-    #         account="tw61901.us-east-1",
-    #         authenticator='externalbrowser',
-    #         role="TECHNOLOGY",
-    #         warehouse="HUMAN_WH",
-    #         database="PROD_PLATFORM_LOCAL")
-    # print("Got sfConnection")
-    # return sfConnection
-    SF_DB_USERNAME = "kyonamine@yext.com"
-    SF_DB_PASSWORD = ""
-    sfConnection = sf.connect(
-        user = SF_DB_USERNAME,
-        account = "exa52539.us-east-1",
-        authenticator = 'externalbrowser',
-        role = "TECHNOLOGY",
-        warehouse = "HUMAN_WH",
-        database = "PROD_PLATFORM_LOCAL")
-    f'{sfConnection}'
-    return sfConnection
+    conn = st.connection("snowflake")
+    df = conn.query("select * from YEXT_DATALAKE.DB4_ALPHA.TAGS_LISTINGS where 1  and partner_id = 638 and location_id in (62365949)")
+    for row in df.itertuples():
+        st.write(f'{row}')
+    return
 
 printout()
-# connectToSF()
-
-
 snowflake()
