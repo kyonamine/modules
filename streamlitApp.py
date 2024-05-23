a3b8f5481ab2f43170de9f512c7070ca4ebe14d6
Kevin Yonamine <kyonamine@yext.com>
2024-05-23 15:08:43 -0400

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -8,7 +8,7 @@ def printout():
     f'{firstFile.func()} {secondFile.func()}'
 
 def snowflake():
-    f'{st.secrets["snowflake"]}'
+    f'{st.secrets["password"]}'
     conn = st.connection("snowflake")
     df = conn.query("select * from YEXT_DATALAKE.DB4_ALPHA.TAGS_LISTINGS where 1  and partner_id = 638 and location_id in (62365949)")
     for row in df.itertuples():
